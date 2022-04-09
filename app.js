d616bc3e6c24929c03d2b17e516db75d8a14df5a
LeThanhTung0707 <tenanhlavip@gmail.com>
2022-04-09 20:14:35 +0700

    Add bug and base views

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -12,6 +13,9 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('././routes/reviewRoutes');
 const globalErrorHandler = require('./controllers/errorController');
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(`${__dirname}/public`));
 app.use(helmet());
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
@@ -29,7 +33,6 @@ app.use(xss());
 app.use(hpp());
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -37,6 +40,11 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
+app.get('/', (req, res, next) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker'
+  });
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
